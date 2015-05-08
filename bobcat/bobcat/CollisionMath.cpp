a591a925aabec0dd0356230b63e5381db2f1a50a
Coteh <james_c_34@live.com>
2015-05-08 19:28:41 -0400

    Add AABB Box to Sphere collision. Add reflect vector method as a form of collision response.

--- bobcat/bobcat/CollisionMath.cpp
+++ bobcat/bobcat/CollisionMath.cpp
@@ -7,6 +7,14 @@ bool CollisionMath::CheckForCollision(Collider* _c1, Collider* _c2){
 	if (_c1->colliderType == ColliderType::SPHERE){
 		if (_c2->colliderType == ColliderType::SPHERE){
 			return CheckForSphereToSphereCollision((SphereCollider*)_c1, (SphereCollider*)_c2);
+		} else if (_c2->colliderType == ColliderType::BOX){
+			return CheckForAABBToSphereCollision((BoxCollider*)_c2, (SphereCollider*)_c1);
+		}
+	} else if (_c1->colliderType == ColliderType::BOX){
+		if (_c2->colliderType == ColliderType::BOX){
+			return CheckForAABBToAABBCollision((BoxCollider*)_c1, (BoxCollider*)_c2);
+		} else if (_c2->colliderType == ColliderType::SPHERE){
+			return CheckForAABBToSphereCollision((BoxCollider*)_c1, (SphereCollider*)_c2);
 		}
 	}
 	return false; //not implemented yet
@@ -17,4 +25,59 @@ bool CollisionMath::CheckForSphereToSphereCollision(SphereCollider* _sc1, Sphere
 
 	//If the distance between the two spheres is less than or equal to the sum of both radii, then there is a collision
 	return (distance <= _sc1->radius * _sc1->gameObject->transform->scale.x + _sc2->radius * _sc2->gameObject->transform->scale.x);
+}
+
+bool CollisionMath::CheckForAABBToAABBCollision(BoxCollider* _bc1, BoxCollider* _bc2){
+	return false; //not implemented yet
+}
+
+bool CollisionMath::CheckForAABBToSphereCollision(BoxCollider* _bc, SphereCollider* _sc){
+	//Get the opposing corners of the cube
+	glm::vec3 boxPos = _bc->gameObject->transform->position;
+	glm::vec3 spherePos = _sc->gameObject->transform->position;
+	glm::vec3 boxScale = _bc->gameObject->transform->scale;
+	glm::vec3 corner1 = glm::vec3(boxPos.x - (_bc->width * boxScale.x) / 2, boxPos.y - (_bc->height * boxScale.y) / 2, boxPos.z - (_bc->length * boxScale.z) / 2);
+	glm::vec3 corner2 = glm::vec3(boxPos.x + (_bc->width * boxScale.x) / 2, boxPos.y + (_bc->height * boxScale.y) / 2, boxPos.z + (_bc->length * boxScale.z) / 2);
+	
+	//Find the square of the distance from the sphere to the box
+	float radius = (_sc->radius * _sc->gameObject->transform->scale.x);
+	float distanceSquared = ::pow(radius, 2);
+
+	if (spherePos.x < corner1.x){
+		distanceSquared -= ::pow(spherePos.x - corner1.x, 2);
+	} else if (spherePos.x > corner2.x){
+		distanceSquared -= ::pow(spherePos.x - corner2.x, 2);
+	}
+	if (spherePos.y < corner1.y){
+		distanceSquared -= ::pow(spherePos.y - corner1.y, 2);
+	} else if (spherePos.y > corner2.y){
+		distanceSquared -= ::pow(spherePos.y - corner2.y, 2);
+	}
+	if (spherePos.z < corner1.z){
+		distanceSquared -= ::pow(spherePos.z - corner1.z, 2);
+	} else if (spherePos.z > corner2.z){
+		distanceSquared -= ::pow(spherePos.z - corner2.z, 2);
+	}
+	return (distanceSquared > 0);
+}
+
+glm::vec3 CollisionMath::ReflectVector(glm::vec3 _v, glm::vec3 _n){
+	//Save vector length for later
+	float vLength = glm::length(_v);
+
+	//Normalize the vectors
+	glm::vec3 v = glm::normalize(_v);
+	glm::vec3 n = glm::normalize(_n);
+
+	//r is the output vector
+	glm::vec3 r = v;
+
+	//Use the dot product to figure out the "reflected" vector
+	r = -2 * glm::dot(v, n) * n + v;
+
+	//Restore length to reflected vector
+	r *= vLength;
+
+	//Return the reflected vector
+	return r;
 }
--- bobcat/bobcat/CollisionMath.h
+++ bobcat/bobcat/CollisionMath.h
@@ -13,11 +13,16 @@ namespace bobcat {
 		* Check for a collision between two colliders.
 		* @param _c1 First collider.
 		* @param _c2 Second collider.
-		* @note This method has not been implemented yet. Do not use.
 		*/
 		static bool CheckForCollision(Collider* _c1, Collider* _c2);
 
 		static bool CheckForSphereToSphereCollision(SphereCollider* _sc1, SphereCollider* _sc2);
+
+		static bool CheckForAABBToAABBCollision(BoxCollider* _bc1, BoxCollider* _bc2);
+
+		static bool CheckForAABBToSphereCollision(BoxCollider* _bc, SphereCollider* _sc);
+
+		static glm::vec3 ReflectVector(glm::vec3 _v, glm::vec3 _n);
 	};
 
 }
